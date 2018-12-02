186abb3a091a62a4ee45bf6da20a88349f831442
Ryhan Chowdhury <ryhan.chowdhury3@gmail.com>
2018-12-01 18:52:27 -0600

    more changes

--- src/urlSubmit.js
+++ src/urlSubmit.js
@@ -2,6 +2,8 @@ import slurs from "./constants"
 const cheerio = require('cheerio')
 
 export default function handleURLSubmit(e) {
+  const slurs = process.env.REACT_APP_SLURS
+  const slurs_array = slurs.split("-")
   fetch(`https://cors-anywhere.herokuapp.com/${e.target[0].value}`)
     .then(response => response.text())
     .then(body => {
